16bfe21279c364e2bca47113481238f567600928
unknown <nikola.vasic01@gmail.com>
2023-10-08 12:00:32 +0200

    Prva faza

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    // const headerStyle= {
+    //     backgroundColor: 'mediumblue',
+    //     color: '#fff'
+    // }
+  return (
+    <header>
+        <h1>Groceries List</h1>
+    </header>
+  )
+}
+
+export default Header
