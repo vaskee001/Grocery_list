e4bde9de968b20b56cee842b12d5f8bb654dae43
unknown <nikola.vasic01@gmail.com>
2023-10-24 18:38:17 +0200

    Dodate funkcionalnosti

--- src/ItemList.js
+++ src/ItemList.js
@@ -6,7 +6,6 @@ const ItemList = ({ items, handleCheck, handleDelete }) => {
     <ul>
       {items.map((item) => (
         <LineItem
-          key={item.id}
           item={item}
           handleCheck={handleCheck}
           handleDelete={handleDelete}
